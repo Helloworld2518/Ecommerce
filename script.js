d2f8e98f7f033fd756d049a861d6aa02cb9071dd
Helloworld2518 <ekaadi0906@gmail.com>
2024-06-16 14:39:39 +0700

    uji 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//scroll code product
+
 function kanan1() {
   var kanan = document.querySelector(".kartu1");
   kanan.scrollBy(350, 0);
@@ -15,8 +17,57 @@ function kiri2() {
   kiri.scrollBy(-350, 0);
 }
 
+//Total Belanjaan
+var harga = 0;
+function akhir() {
+  var total = (document.getElementsByClassName("totalan")[0].textContent =
+    "Rp. " + harga);
+}
+
+//fungsi penambahan
+function harga1() {
+  var check = document.getElementsByClassName("barang1");
+  var barang = Number(document.getElementsByClassName("barang1")[0].value);
+
+  if (check[0].checked == true) {
+    harga += barang;
+    akhir();
+  } else if (check[0].checked == false) {
+    harga -= barang;
+    akhir();
+  }
+}
+function harga2() {
+  var check = document.getElementsByClassName("barang1");
+  var barang = Number(document.getElementsByClassName("barang1")[1].value);
+
+  if (check[1].checked == true) {
+    harga += barang;
+    akhir();
+  } else if (check[1].checked == false) {
+    harga -= barang;
+    akhir();
+  }
+}
+
+function harga3() {
+  var check = document.getElementsByClassName("barang1");
+  var barang = Number(document.getElementsByClassName("barang1")[2].value);
+
+  if (check[2].checked == true) {
+    harga += barang;
+    akhir();
+  } else if (check[2].checked == false) {
+    harga -= barang;
+    akhir();
+  }
+}
+
 function send() {
+  var nama = document.getElementById("username").value;
+  var rekening = document.getElementById("norek").value;
+  var pesan = "Nama: ${nama}/n Nomor rekening: ${rekening}";
   var nomor = "+6281901242626";
-  var value = "https://wa.me/" + nomor + "?text=" + "hello";
+  var value = "https://wa.me/" + nomor + "?text=" + pesan;
   window.open(value, "_blank").focus();
 }
